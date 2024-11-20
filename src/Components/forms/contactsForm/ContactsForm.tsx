838e788aa6b6b1b6bbc6879870e133a700c4121d
tommy <tommy.mccann001@gmail.com>
2024-11-20 15:02:53 +0000

    "Refactored footer styles; added FormContainer component with temporary inline styles for positioning."

---
+++ src/Components/forms/contactsForm/ContactsForm.tsx
@@ -0,0 +1,20 @@
+import "react-datepicker/dist/react-datepicker.css";
+import FormContainer from "../formContainer/FormContainer";
+
+const ContactsForm = () => {
+  return (
+    <>
+      <FormContainer>
+        <div
+          style={{
+            height: "600px",
+            width: "50%",
+            backgroundColor: "blueviolet",
+          }}
+        ></div>
+      </FormContainer>
+    </>
+  );
+};
+
+export default ContactsForm;
--- src/pages/contacts/ContactsPage.tsx
+++ src/pages/contacts/ContactsPage.tsx
@@ -1,7 +1,15 @@
+import ContactsForm from "../../Components/forms/contactsForm/ContactsForm";
+
 const ContactsPage = () => {
   return (
     <>
-      <h2>Contacts</h2>
+      <ContactsForm />
+      {/* import form re-usable form container this should include default style for all forms */}
+      {/* make form  */}
+      {/* vlidate form data */}
+      {/* handle form data */}
+      {/* contacts list */}
+      {/* test for form */}
     </>
   );
 };
